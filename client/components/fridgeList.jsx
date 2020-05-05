5480154abdaa5e613f81830b9a0ae3301fd430d0
Johnny Ung <johnnyung91@gmail.com>
2020-05-05 00:07:42 +0000

    added new ingredient component to populate list items

--- client/components/fridgeList.jsx
+++ client/components/fridgeList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Ingredient from './ingredient';
 
 export default class RecipeList extends React.Component {
   constructor(props) {
@@ -9,8 +10,52 @@ export default class RecipeList extends React.Component {
   }
 
   render() {
+    const dummy = [
+      {
+        ingredientId: 1,
+        name: 'chicken',
+        userId: 1
+      },
+      {
+        ingredientId: 2,
+        name: 'beef',
+        userId: 1
+      },
+      {
+        ingredientId: 3,
+        name: 'pork',
+        userId: 1
+      },
+      {
+        ingredientId: 4,
+        name: 'onion',
+        userId: 1
+      },
+      {
+        ingredientId: 5,
+        name: 'carrot',
+        userId: 1
+      }
+    ];
+
+    const ingredients = dummy.map(ingredient => {
+      return (
+        <Ingredient
+          key={ingredient.ingredientId}
+          ingredient={ingredient}
+        />
+      );
+    });
+
     return (
-      <h1 className='testing'>FRIDGE PAGE</h1>
+      <div className="container">
+        <div className="form my-3 px-3">
+          <input className="form-control" type="text" placeholder="Default input"/>
+        </div>
+        <ul className="list-group list-group-flush">
+          {ingredients}
+        </ul>
+      </div>
     );
   }
 }
