150920d028197df5bb6a3ac4141c5409d30197f3
Steven <hellosteventran@gmail.com>
2020-06-27 04:11:15 +0000

    Completed Intro modal

--- client/components/introModal.jsx
+++ client/components/introModal.jsx
@@ -7,11 +7,11 @@ export default class IntroModal extends React.Component {
       <div className="introModalOpacity">
         <div className="modalDiv container">
           <div className="introModal">
-            <h3 className="bonApetit">Welcome to Get Cookin&apos;!</h3>
-            <p className="doneCookingText">This application is best viewed on a mobile device</p>
-
-            <a href="https://www.youtube.com/watch?v=psg8b3aEQso" target="_blank" rel="noopener noreferrer"><button className="doneCookingButton">View Demo</button></a>
-            <button onClick={() => this.props.handleModalFunction()} className="doneCookingButton">Continue</button>
+            <p className="intro-logo">GET COOKIN&apos;</p>
+            <center><div className="intro-divider"></div></center>
+            <p className="intro-modal-text">Get Cookin&apos; is a dynamic web application made for home cooks who want to use up the items in their pantry.</p>
+            <strong><p className="intro-modal-text">For a video tutorial on how to use Get Cookin&apos;, <a className="demo-link" href="https://www.youtube.com/watch?v=psg8b3aEQso" target="_blank" rel="noopener noreferrer">click here!</a></p></strong>
+            <button onClick={() => this.props.handleModalFunction()} className="doneCookingButton">CONTINUE</button>
           </div>
         </div>
       </div>
